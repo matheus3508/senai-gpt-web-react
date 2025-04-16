db738556d0344bf5abf6cd7b9483182d68fad32e
Matheus <math.olivribeiro@gmail.com>
2025-04-15 21:43:07 -0300

    ..

--- senai-gpt-web-react/src/pages/login/index.jsx
+++ senai-gpt-web-react/src/pages/login/index.jsx
@@ -2,11 +2,40 @@ import "./login.css";
 
 function Login() {
 
-    return (
-      <>
-       <h1>Login</h1>
-      </>
-    )
-  }
-  
-  export default Login;
+  return (
+    <>
+      <body>
+
+        <header></header>
+
+        <main className="page-container">
+
+          <div className="robo-image">
+
+
+          </div>
+
+          <div className="login-container">
+
+            <img className="logo" src="../assets/imgs/Chat.png" alt="Senai GPT - Logo" />
+
+
+            <h1 id="meutitulo" className="titulo">Login</h1>
+
+            <input className="inpt" type="email" placeholder="Insira o e-mail" />
+            <input className="inpt" type="password" placeholder="Insira a senha" />
+
+
+            <button className="btn">Entrar</button>
+          </div>
+
+        </main>
+
+        <footer></footer>
+
+      </body>
+    </>
+  )
+}
+
+export default Login;
