6a1414918d32f7239c004dd7a47406d7083dfeae
Matheus <math.olivribeiro@gmail.com>
2025-04-17 19:21:43 -0300

    react

--- senai-gpt-web-react/src/pages/login/index.jsx
+++ senai-gpt-web-react/src/pages/login/index.jsx
@@ -1,65 +1,62 @@
 import "./login.css";
-
 import logo from "../../assets/imgs/Chat.png";
 import { useState } from "react";
-
-
 function Login() {
 
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const onLoginClick = async () => {
+  const onLoginClick = async ()=> {
 
-    let response = await fetch ("https//senai-gpt-api.azzurewebsites.net/login", {
+    let response = await fetch("https://senai-gpt-api.azurewebsites.net/login", {
+
       headers: {
-        "Content-Type" : "application/json"
+        "Content-Type": "application/json"
       },
-      method: "POST", //metodo que envia
-      body: JSON.stringify({
+      method: "POST",
+      body: JSON.stringify( {
         email: email,
-        password: password
+        password:password
       })
+
     });
 
     console.log(response)
-    
+
   }
+
   return (
     <>
-      <body>
+      <header></header>
 
-        <header></header>
+      <main className="page-container">
+        <div className="robo-image"></div>
 
-        <main className="page-container">
+        <div className="login-container">
+          <img
+            className="logo"
+            src={logo}
+            alt="SenaiGPT logo"
+          />
 
-          <div className="robo-image">
+          <h1 id="meutitulo" className="titulo">
+            Login
+          </h1>
 
+          <input className="inpt" value={email} onChange={event => setEmail(event.target.value)} type="email" placeholder="Insira o e-mail" />
+          <input
+            className="inpt" value={password} onChange={event => setPassword(event.target.value)}
+            type="password"
+            placeholder="Insira a senha"
+          />
 
-          </div>
+          <button className="btn" onClick={() => onLoginClick()}>Entrar</button>
+        </div>
+      </main>
 
-          <div className="login-container">
-
-            <img className="logo" src={logo} alt="Senai GPT - Logo" />
-
-
-            <h1 id="meutitulo" className="titulo">Login</h1>
-
-            <input className="inpt" value={email} onChange={event => setEmail(event.target.value)} type="email" placeholder="Insira o e-mail" />
-            <input className="inpt" value={password} onChange={event => setPassword(event.target.value)} type="password" placeholder="Insira a senha" />
-
-         
-
-            <button className="btn" onClick={() => onLoginClick()}>Entrar</button>
-          </div>
-
-        </main>
-
-        <footer></footer>
-
-      </body>
+      <footer></footer>
     </>
-  )
+  );
 }
 
 export default Login;
